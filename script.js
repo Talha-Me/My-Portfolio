0255c04c86e27e61a7c5a4f90e1978c98003f832
Talha <abutalhaakash26@gmail.com>
2026-03-26 20:54:36 +0600

    all change

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
+        strings: [ "IT Officer","Developer", "Blogger", "Designer", "Freelancer"],
+        typeSpeed: 10,
+        backSpeed: 50,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["IT Officer","Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
